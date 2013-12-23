7b584a824c6e12962699f2a0f989b4e4a2c19e47
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 18:10:42 +0530

    example17

---
+++ ex17.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+a, sfname, dfname = argv
+
+print "copying file from %s to %s" % (sfname, dfname)
+
+infile = open(sfname)
+indata = infile.read()
+
+print "size of input file : %d bytes" % len(indata)
+
+print "output file exists? %r" % exists(dfname)
+print "enter to continue"
+raw_input()
+
+outfile = open(dfname, 'w')
+outfile.write(indata)
+
+print "successfully copied"
+
+infile.close()
+outfile.close()
