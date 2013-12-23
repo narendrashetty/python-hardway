a7dea7ef35af0dc9f223ee70e3778725a8d8c23d
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 14:55:52 +0530

    example2

---
+++ ex2.py
@@ -0,0 +1,12 @@
+#start of the program
+#printing on the output screen
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+#and it continues
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this'
+print "custom"
+#finally its done
