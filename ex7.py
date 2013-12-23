36be2b38f3f50a43a76000db929839978e1cdbd6
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 16:12:40 +0530

    example7

---
+++ ex7.py
@@ -0,0 +1,28 @@
+#prints line
+print "print line 1."
+#prints line with a string
+print "print with string: %s." % 'snow'
+print "line 2"
+
+print "." * 10  #prints the string 10 times
+
+#stores a character in each variable - 12 variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#comma used to seperate it by adding space on output screen
+print end1 + end2 + end3 + end4 + end5 + end6, end7 + end8 + end9 + end10 + end11 + end12
+
+#another way to do it
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
