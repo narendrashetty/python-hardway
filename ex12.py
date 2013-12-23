3f91f7ce19294996b1ee8c3c4b835e09b26536b3
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 17:01:08 +0530

    example12

---
+++ ex12.py
@@ -0,0 +1,13 @@
+#read input
+
+age = raw_input("enter age ")
+
+height = raw_input("enter height ")
+
+weight = raw_input("enter weight ")
+
+#using %s
+print "so you are %s years old, %s tall and %s heavy" % (age, height, weight);
+
+#using %r
+print "so you are %r years old, %r tall and %r heavy" % (age, height, weight);
