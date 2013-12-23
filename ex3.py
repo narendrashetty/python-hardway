0dfc2ffb4c94e7521602258eca2657b231da9eb4
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 15:12:19 +0530

    example3

---
+++ ex3.py
@@ -0,0 +1,37 @@
+#prints a line
+print "I will now count my chickens:"
+
+#count Hens & Roosters
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+#print again
+print "Now I will count the eggs:"
+
+#display the result
+print 3 + 2 + 1 -5 +4 % 2 - 1 / 4+ 6
+
+#print again
+print "Is it true that  3 + 2 < 5 - 7"
+
+#calculate and display
+print 3 + 2 < 5 - 7
+
+#prints predefined and after calculation
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+#prints predefined and after calculation (floating)
+print "What is 3.2 + 2.2?", 3.2 + 2.2
+print "What is 5.2 - 7.1?", 5.2 - 7.1
+
+#print again
+print "Oh, that's why it's False."
+
+#print again
+print "How about some more."
+
+#prints predefined and after calculation
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it lesser or equal?", 5 <= -2
