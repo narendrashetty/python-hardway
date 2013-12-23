5ed93d9da26cc90bbb43e5212b0211c51f1ab9c3
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 16:29:01 +0530

    example9

---
+++ ex9.py
@@ -0,0 +1,15 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApril\nMay\nJune\nJuly\nAug\nSept\nOct\nNov\nDec"
+
+print "Days ", days
+print "Months ", months
+
+print """
+multiple line print
+multiple line goes on
+and on 
+and onnn
+"""
+#prints for debugging
+print "using Raw months %r" % months
+
