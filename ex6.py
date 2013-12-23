64d9429b6f14f2e74ac1636931c6938e3af111e2
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 16:03:24 +0530

    example6

---
+++ ex6.py
@@ -0,0 +1,31 @@
+#creating a string variable with formats %d
+x = "There are %d types of people." % 10
+#creating a string variable 'binary
+binary = "binary"
+#and another string variable
+do_not = "don't"
+
+#creating a variable y using the above 2 strings by placing it using string formats 
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+#printing both x and y in different lines
+print x
+print y
+
+#using %r and %s where %r encloses string with '' but %s doesnt
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+#creating a boolean variable
+hilarious = False
+#and a string with format %r
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+#combining both in 1 variable and printing it
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = " a string with a right side."
+
+#concatination of both string w and e using + operator and printing it
+print w + e
