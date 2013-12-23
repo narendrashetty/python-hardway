af1dde7ee5b24a7bc7f8ceee581b80767ae6c8be
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 16:40:25 +0530

    example10

---
+++ ex10.py
@@ -0,0 +1,20 @@
+#escape sequence
+
+#\a gives a beep sond called bell
+a = "tab\tspace "
+b = "new\nline"
+
+#\b backspace
+c = "back \\ s\blash"
+
+d = """
+escape in multiple line
+\t tab
+\\ backslash
+\n new line
+"""
+
+print a
+print b
+print c
+print d
