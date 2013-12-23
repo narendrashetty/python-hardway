743ebb28e3adedd9d1e5f789fead6f13d05349ce
narendrashetty <mailnarendra15@gmail.com>
2013-12-23 16:14:48 +0530

    comment updated

--- ex7.py
+++ ex7.py
@@ -20,7 +20,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-#comma used to seperate it by adding space on output screen
+#comma used to seperate it by adding space on output screen but its a bad practice because its more then 80 character
 print end1 + end2 + end3 + end4 + end5 + end6, end7 + end8 + end9 + end10 + end11 + end12
 
 #another way to do it
