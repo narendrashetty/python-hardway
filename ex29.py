0b74afbc60f89a5d8ae5c01c05603144d579a07d
narendrashetty <mailnarendra15@gmail.com>
2013-12-24 11:58:26 +0530

    Example29

---
+++ ex29.py
@@ -0,0 +1,17 @@
+a = 10
+b = 20
+c = 30
+
+if a < b:
+	print "a is lesser than b"
+
+if c > b:
+	print "c is greater than b"
+	
+b += 10
+
+if b >= c:
+	print "b is greater or equal to c"
+	
+if b == c:
+	print "b is equal to c"
